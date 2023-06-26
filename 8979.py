608b9971d1a7de76d944108f6c41e77efcdac5dd
Yunbada <98579240+Yunbada@users.noreply.github.com>
2023-06-26 16:59:38 +0900

    8979 Clear

--- 8979.py
+++ 8979.py
@@ -12,41 +12,7 @@
 #출력 예제
 #2
 
-#병합정렬 알고리즘 생성
-def merge_sort(arr):
-    def sort(low, high):
-        if high - low < 2:
-            return
-        mid = (low + high) // 2
-        sort(low, mid)
-        sort(mid, high)
-        merge(low, mid, high)
-
-    def merge(low, mid, high):
-        temp = []
-        l, h = low, mid
-
-        while l < mid and h < high:
-            if arr[l] < arr[h]:
-                temp.append(arr[l])
-                l += 1
-            else:
-                temp.append(arr[h])
-                h += 1
-
-        while l < mid:
-            temp.append(arr[l])
-            l += 1
-        while h < high:
-            temp.append(arr[h])
-            h += 1
-
-        for i in range(low, high):
-            arr[i] = temp[i - low]
-
-    return sort(0, len(arr))
-
-#국가의 수 & 몇등인지 입력
+#국가의 수 & 국가 번호 입력
 c,r = map(int,input().split())
 #국가순서대로 금은동 메달 갯수
 clist = []
@@ -54,5 +20,24 @@ for i in range(c):
     clist.append(list(map(int,input().split())))
 
 #순위 찾기
-merge_sort(clist[?][1])
-#어캐풀어 이거;
+clist.sort(key = lambda x:(-x[1],-x[2],-x[3]))
+rank = 1
+#print(clist)
+for i in range(c):
+    if clist[i][0] == r:
+        r = i
+        break
+    
+for i in range(c):
+    if i != r:
+        if clist[i][1] > clist[r][1]:
+            rank += 1
+        elif clist[i][1] == clist[r][1]:
+            if clist[i][2] > clist[r][2]:
+                rank += 1
+            elif clist[i][2] == clist[r][2]:
+                if clist[i][3] > clist[r][3]:
+                    rank += 1
+print(rank)
+
+#정답입니다 얏호~
