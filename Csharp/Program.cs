f5d9fac6d96a048869df622897ec712d6c56b37b
Yunbada <98579240+Yunbada@users.noreply.github.com>
2023-07-12 17:49:50 +0900

    C# On

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int[] arr = new int[5];
+for(int i = 0; i < arr.Length; i++)
+{
+   Console.Write("0", arr[i]);
+   Debug.Log("hello");
+}
